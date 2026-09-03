9d7429b3a7b8b5c8ad9c4200c0fc90c2219733dd
GeoSarmiento <jhova29@gmail.com>
2026-09-03 22:00:41 +0200

    Add portfolio application

---
+++ my-hv/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <h1>Hola Geo</h1>
+      </section>
+
+   
+     
+    </>
+  )
+}
+
+export default App

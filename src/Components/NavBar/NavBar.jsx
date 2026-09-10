35c9e93cb1281d6b5ad66d1a8181f4fcc9640120
GeoSarmiento <jhova29@gmail.com>
2026-09-10 12:07:02 +0200

    mejoras_menu

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { useState } from 'react';
 import './NavBar.scss';
-import { Menu } from 'lucide-react';
+import { Menu, X } from 'lucide-react';
 
 
 const NavBar = () => {
@@ -16,23 +16,38 @@ const NavBar = () => {
 
         {/* MENÚ DE NAVEGACIÓN */}
         <ul className={`navbar-menu ${isOpen ? 'is-active' : ''}`}>
+          
+          <div className="closed">
+          <Link to="/"  onClick={() => setIsOpen(true)}><X/></Link>
+          </div>
 
             <Link to="/"          onClick={() => setIsOpen(true)}>Home  </Link>
             <Link to="/about"     onClick={() => setIsOpen(true)}>About</Link>
             <Link to="/projects"  onClick={() => setIsOpen(true)}>Projects</Link>
             <Link to="/contact"   onClick={() => setIsOpen(true)}>Contact</Link>
+      
+      <div className="redes">
+      <span>Ins</span>
+      <span>Face</span>
+
+      <span>Github</span>
+
+      <span>Link</span>
+
+    </div>
       </ul>
+      
 
         {/* BOTÓN HAMBURGUESA  */ }
       
         
-        <Menu 
+        <Menu color="white" width={32}
           className={`navbar-toggle ${isOpen ? 'is-active' : ''}`}
           onClick={() => setIsOpen(!isOpen)}
           />
       </nav>
 
-
+    
       
  
     </header>

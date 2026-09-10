6d4dee266b62280385b04cd26d368725f301c023
GeoSarmiento <jhova29@gmail.com>
2026-09-10 19:50:06 +0200

    redes

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,10 +1,28 @@
-import "./Contact.css";
+import "./Contact.scss";
 
 
 const Contact = () => {
   return (
     <div className="contact-container">
-      <h1>Contact</h1>
+      
+      <div className="intro_contact">
+         <h1> Let's Talk</h1>
+
+        <p> Have a project in mind? 
+          We'd love to hear from you. Send us a message and we'll 
+          respond as soon as possible.
+        </p>
+      </div>
+
+
+
+
+      <form>
+      <h2>Send Message</h2>
+
+      </form>
+
+
     </div>
   )
 }

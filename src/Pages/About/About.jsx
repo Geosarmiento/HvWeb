d1c81b3a408526fe752a4927d24b94a18fb79ac3
GeoSarmiento <jhova29@gmail.com>
2026-09-10 21:30:49 +0200

    mejorasnav

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,18 +1,53 @@
-import './About.css'
+import './About.scss'
 
 const About = () => {
   return (
 
 
-    <div className='about-container' id='about'
-   
-     
-        >
+    <div className='about-container' id='about'>
+      <div className="photo_perfil"> 
+      </div>
+    <br /> <br />
+
 
 
       <h1>About Me</h1>
       <p>Multidisciplinary Graphic Designer and Frontend Designer with experience in visual communication, digital interface design, frontend development support, 3D visualization and graphic production. Technologist in Multimedia Production with a practical profile combining design, technology, 3D and hands-on production.</p>
-    
+    <br /> <br /> 
+      <h2>Skills</h2>
+
+    <div className="skills">
+      <span>IA</span>
+      <span>3D</span>
+      <span>IA</span>
+      <span>3D</span>
+      <span>IA</span>
+      <span>3D</span>
+      <span>IA</span>
+      <span>3D</span>
+      <span>IA</span>
+      <span>3D</span>
+      <span>IA</span>
+      <span>3D</span>
+      <span>IA</span>
+      <span>3D</span>
+      <span>IA</span>
+      <span>3D</span>
+      <span>IA</span>
+      <span>3D</span>
+      <span>IA</span>
+      <span>3D</span>
+      <span>IA</span>
+      <span>3D</span>
+      <span>IA</span>
+      <span>3D</span>
+      <span>IA</span>
+      <span>3D</span>
+      <span>IA</span>
+      <span>3D</span>
+      <span>IA</span>
+    </div>
+
     </div>
   )
 }
